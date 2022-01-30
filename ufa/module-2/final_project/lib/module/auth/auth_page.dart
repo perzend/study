c661145e3c14c9ae824c3fb1f10c0e47829716e9
perzend <boss.pahan@gmail.com>
2022-01-30 19:10:18 +0300

    Добавлена проверка количества введенных в текстовые поля символов

--- ufa/module-2/final_project/lib/module/auth/auth_page.dart
+++ ufa/module-2/final_project/lib/module/auth/auth_page.dart
@@ -3,7 +3,8 @@ import 'package:final_project/services/secure_storage.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
-  const AuthPage({Key? key, required this.phone, required this.pwd}) : super(key: key);
+  const AuthPage({Key? key, required this.phone, required this.pwd})
+      : super(key: key);
   static const String routeName = '/authPage';
 
   final String? phone;
@@ -19,6 +20,8 @@ class _AuthPageState extends State<AuthPage> {
 
   final SecureStorage _secureStorage = SecureStorage();
 
+  bool _errorPhoneValidate = false;
+  bool _errorPwdValidate = false;
 
   @override
   void initState() {
@@ -34,155 +37,204 @@ class _AuthPageState extends State<AuthPage> {
     super.dispose();
   }
 
+  bool _checkTextOnChanged(String text, int count) {
+    if (text.length < count) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Please, log in\nor register',
-            style: Theme.of(context).textTheme.headline4,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _phoneController,
-              decoration: InputDecoration(
-                labelText: 'Type a phone number',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Please, log in\nor register',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  SizedBox(height: 8,),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                        controller: _phoneController,
+                        style: TextStyle(
+                            fontSize: 28.0,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 3),
+                        decoration: InputDecoration(
+                          contentPadding:
+                              EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                          prefixText: '+7 ',
+                          labelText: 'Type a phone number',
+                          labelStyle: TextStyle(fontSize: 16.0, letterSpacing: 1),
+                          errorText:
+                              _errorPhoneValidate ? 'Must be at least 10 digits' : null,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                        keyboardType: TextInputType.phone,
+                        textInputAction: TextInputAction.next,
+                        maxLength: 10,
+                        onChanged: (text) {
+                          setState(() {
+                            _errorPhoneValidate = _checkTextOnChanged(text, 10);
+                          });
+
+                        }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _pwdController,
+                      style: TextStyle(
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 3),
+                      decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                        labelText: 'Type a password',
+                        labelStyle: TextStyle(fontSize: 16.0, letterSpacing: 1),
+                        errorText:
+                            _errorPwdValidate ? 'Must be at least 6 digits' : null,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      keyboardType: TextInputType.visiblePassword,
+                      textInputAction: TextInputAction.done,
+                      obscureText: true,
+                      obscuringCharacter: '*',
+                      onChanged: (text) {
+                        setState(() {
+                          _errorPwdValidate = _checkTextOnChanged(text, 6);
+                        });
+
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 32,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 16.0, right: 8.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              print(_phoneController.text);
+                              print(widget.phone);
+                              print(_pwdController.text);
+                              print(widget.pwd);
+                              if (_phoneController.text == widget.phone &&
+                                  _pwdController.text == widget.pwd) {
+                                _secureStorage.writeSecureData('login_status', 'isOK');
+                                Navigator.pushReplacementNamed(
+                                    context, Routes.usersList);
+                              } else {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: Text('Login error!',
+                                            textAlign: TextAlign.center,
+                                            style:
+                                                Theme.of(context).textTheme.headline5),
+                                        content: Text(
+                                          'The phone number or password is entered incorrectly or does not exist. Please try again.',
+                                          style: Theme.of(context).textTheme.bodyText1,
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: const Text('OK'))
+                                        ],
+                                      );
+                                    });
+                              }
+                            },
+                            child: Text('Login'),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8.0, right: 16.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_phoneController.text == widget.phone) {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: Text('Registration failed!',
+                                            textAlign: TextAlign.center,
+                                            style:
+                                                Theme.of(context).textTheme.headline5),
+                                        content: Text(
+                                          'This phone number already use.',
+                                          style: Theme.of(context).textTheme.bodyText1,
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: const Text('OK'))
+                                        ],
+                                      );
+                                    });
+                              } else {
+                                _secureStorage.writeSecureData(
+                                    'phone', _phoneController.text);
+                                _secureStorage.writeSecureData(
+                                    'pwd', _pwdController.text);
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: Text('Registration was successful!',
+                                            textAlign: TextAlign.center,
+                                            style:
+                                                Theme.of(context).textTheme.headline5),
+                                        content: Text(
+                                          'Now you will be redirected to the main page of the application.',
+                                          style: Theme.of(context).textTheme.bodyText1,
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () {
+                                                _secureStorage.writeSecureData(
+                                                    'login_status', 'isOK');
+                                                Navigator.pushReplacementNamed(
+                                                    context, Routes.usersList);
+                                              },
+                                              child: const Text('OK'))
+                                        ],
+                                      );
+                                    });
+                              }
+                            },
+                            child: Text('Register'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              keyboardType: TextInputType.phone,
-              maxLength: 11,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _pwdController,
-              decoration: InputDecoration(
-                labelText: 'Type a password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-              ),
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              obscuringCharacter: '*',
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 16.0, right: 8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print(_phoneController.text);
-                      print(widget.phone);
-                      print(_pwdController.text);
-                      print(widget.pwd);
-                      if (_phoneController.text == widget.phone &&
-                          _pwdController.text == widget.pwd) {
-                        _secureStorage.writeSecureData('login_status', 'isOK');
-                        Navigator.pushReplacementNamed(
-                            context, Routes.usersList);
-                      } else {
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: Text('Login error!',
-                                    textAlign: TextAlign.center,
-                                    style:
-                                        Theme.of(context).textTheme.headline5),
-                                content: Text(
-                                  'The phone number or password is entered incorrectly or does not exist. Please try again.',
-                                  style: Theme.of(context).textTheme.bodyText1,
-                                ),
-                                actions: [
-                                  TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: const Text('OK'))
-                                ],
-                              );
-                            });
-                      }
-                    },
-                    child: Text('Login'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8.0, right: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_phoneController.text == widget.phone) {
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: Text('Registration failed!',
-                                    textAlign: TextAlign.center,
-                                    style:
-                                        Theme.of(context).textTheme.headline5),
-                                content: Text(
-                                  'This phone number already use.',
-                                  style: Theme.of(context).textTheme.bodyText1,
-                                ),
-                                actions: [
-                                  TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: const Text('OK'))
-                                ],
-                              );
-                            });
-                      } else {
-                        _secureStorage.writeSecureData(
-                            'phone', _phoneController.text);
-                        _secureStorage.writeSecureData(
-                            'pwd', _pwdController.text);
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: Text('Registration was successful!',
-                                    textAlign: TextAlign.center,
-                                    style:
-                                        Theme.of(context).textTheme.headline5),
-                                content: Text(
-                                  'Now you will be redirected to the main page of the application.',
-                                  style: Theme.of(context).textTheme.bodyText1,
-                                ),
-                                actions: [
-                                  TextButton(
-                                      onPressed: () {
-                                        _secureStorage.writeSecureData(
-                                            'login_status', 'isOK');
-                                        Navigator.pushReplacementNamed(
-                                            context, Routes.usersList);
-                                      },
-                                      child: const Text('OK'))
-                                ],
-                              );
-                            });
-                      }
-                    },
-                    child: Text('Register'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }
